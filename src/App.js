dbcc31682b86e865a7cbef0b0f41a8bb8d1404e3
Nirav9603 <niravmagatarapara111@gmail.com>
2022-09-01 16:03:27 +0530

    auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./container/home/Home";
+import About from "./container/about/About";
+import Departments from "./container/departments/Departments";
+import Doctors from "./container/doctors/Doctors";
+import Contact from "./container/contact/Contact";
+import Appointment from "./container/appointment/Appointment";
+import {Switch, Route} from "react-router-dom"
+import Auth from './container/auth/Auth';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Switch>
+        <Route path={"/"} exact component={Home} />
+        <Route path={"/departments"} exact component={Departments} />
+        <Route path={"/doctors"} exact component={Doctors} />
+        <Route path={"/about"} exact component={About} />
+        <Route path={"/contact"} exact component={Contact} />
+        <Route path={"/appointment"} exact component={Appointment} />
+        <Route path={"/auth"} exact component={Auth}/>
+      </Switch>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/container/auth/Auth.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Auth(props) {
+
+    const [userType, setUserType] = useState('Login')
+
+    return (
+        <div>
+            <section id="appointment" className="appointment">
+                <div className="container">
+                    <div className="section-title">
+                        {
+                            userType === 'Login' ? <h2>Login</h2> : <h2>Signup</h2>
+                        }
+
+                    </div>
+                    <div action method="post" role="form" className="php-email-form">
+
+                        {
+                            userType === 'Login'
+                                ? null
+                                : <div className="row">
+                                    <div className="col-md-4 form-group">
+                                        <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                        <div className="validate" />
+                                    </div>
+                                </div>
+                        }
+
+
+                        <div className="row">
+                            <div className="col-md-4 form-group mt-3 mt-md-0">
+                                <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                                <div className="validate" />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-4 form-group mt-3 mt-md-0">
+                                <input type="tel" className="form-control" name="password" id="password" placeholder="Your Password" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                <div className="validate" />
+                            </div>
+                        </div>
+                        {
+                            userType === 'Login' ? <div className="text-center"><button type="submit">Login</button></div> : <div className="text-center"><button type="submit">Signup</button></div>
+                        }
+
+                        {
+                            userType === 'Login' ? <div className="text-center mt-2">Create a new account: <Link onClick={() => setUserType('Signup')}>Signup</Link> </div> : <div className='text-center mt-2' >Already have an Acoount? <Link onClick={() => setUserType('Login')}>Login</Link> </div>
+                        }
+
+                    </div>
+                </div>
+            </section>
+        </div>
+
+    );
+}
+
+export default Auth;
