f586fd4b9d09f904eaed87f628dadc2ed3249ed6
Nirav9603 <niravmagatarapara111@gmail.com>
2022-09-02 10:03:14 +0530

    Reset

--- src/container/auth/Auth.js
+++ src/container/auth/Auth.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 function Auth(props) {
 
-    const [userType, setUserType] = useState('Login')
+    const [userType, setUserType] = useState('Login');
+    const [reset, setReset] = useState(false)
 
     return (
         <div>
@@ -11,21 +12,27 @@ function Auth(props) {
                 <div className="container">
                     <div className="section-title">
                         {
-                            userType === 'Login' ? <h2>Login</h2> : <h2>Signup</h2>
+                            reset === true ?
+                                <h2>Reset Password</h2>
+                                :
+                                userType === 'Login' ? <h2>Login</h2> : <h2>Signup</h2>
                         }
 
                     </div>
                     <div action method="post" role="form" className="php-email-form">
 
                         {
-                            userType === 'Login'
-                                ? null
-                                : <div className="row">
-                                    <div className="col-md-6 form-group mx-auto">
-                                        <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                        <div className="validate" />
+                            reset === true ?
+                                null
+                                :
+                                userType === 'Login'
+                                    ? null
+                                    : <div className="row">
+                                        <div className="col-md-6 form-group mx-auto">
+                                            <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                            <div className="validate" />
+                                        </div>
                                     </div>
-                                </div>
                         }
 
 
@@ -35,19 +42,31 @@ function Auth(props) {
                                 <div className="validate" />
                             </div>
                         </div>
-                        <div className="row">
-                            <div className="col-md-6 form-group mt-3 mt-md-0 mx-auto">
-                                <input type="tel" className="form-control" name="password" id="password" placeholder="Your Password" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                <div className="validate" />
-                            </div>
-                        </div>
+
                         {
+                            reset === true ?
+                                null
+                                :
+                                <div className="row">
+                                    <div className="col-md-6 form-group mt-3 mt-md-0 mx-auto">
+                                        <input type="tel" className="form-control" name="password" id="password" placeholder="Your Password" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                        <div className="validate" />
+                                    </div>
+                                </div>
+                        }
+
+                        {
+                            reset === true ?
+                                <div className="text-center"><button type="submit">Submit</button></div>
+                            :
                             userType === 'Login' ? <div className="text-center"><button type="submit">Login</button></div> : <div className="text-center"><button type="submit">Signup</button></div>
                         }
 
                         {
-                            userType === 'Login' ? <div className="text-center mt-2">Create a new account: <Link onClick={() => setUserType('Signup')}>Signup</Link> </div> : <div className='text-center mt-2' >Already have an Acoount? <Link onClick={() => setUserType('Login')}>Login</Link> </div>
+                            userType === 'Login' ? <div className="text-center mt-2">Create a new account: <Link onClick={() => {setReset(false); setUserType('Signup')}}>Signup</Link> </div> : <div className='text-center mt-2' >Already have an Acoount? <Link onClick={() => {setReset(false); setUserType('Login')}}>Login</Link> </div>
                         }
+
+                        <div className="text-center mt-2"><Link onClick={() => setReset(true)}>Forgot Password?</Link></div>
 
                     </div>
                 </div>
