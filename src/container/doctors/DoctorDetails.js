eb2a9bd29876a5d0ed14cbee6b568f43edc6c706
Nirav9603 <niravmagatarapara111@gmail.com>
2022-10-17 12:32:25 +0530

    Doctor-Details

--- src/container/doctors/DoctorDetails.js
+++ src/container/doctors/DoctorDetails.js
@@ -8,15 +8,11 @@ function DoctorDetails(props) {
 
     console.log(data);
 
-    const Data = [
-        { id: "1", name: "Marklin Lora", img: "../assets/img/doctors/doctors-1.jpg", designation: "Chief Medical Officer", description: "Duis sagittis rutrum neque, quis tincidunt arcu pretium ac.",ldescription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", phone: "98756 12345", add: "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016" },
-        { id: "2", name: "John White", img: "../assets/img/doctors/doctors-2.jpg", designation: "Anesthesiologist", description: "Aenean ac turpis ante. Mauris velit sapien.", ldescription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", phone: "98756 12345", add: "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016" },
-        { id: "3", name: "Umika Loha", img: "../assets/img/doctors/doctors-3.jpg", designation: "Cardiology", description: "Curabitur luctus eleifend odio. Phasellus placerat mi.", ldescription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", phone: "98756 12345", add: "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016" },
-        { id: "4", name: "Daimy Smith", img: "../assets/img/doctors/doctors-4.jpg", designation: "Neurosurgeon", description: "Morbi vulputate, tortor nec pellentesque molestie, eros nisi ornare purus.", ldescription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", phone: "98756 12345", add: "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016" }
-    ]
+    
 
     useEffect(()=>{
-        let Ddata = Data.find((l)=>{
+        let Data = JSON.parse(localStorage.getItem("Doctor-Details"))
+        Data.find((l)=>{
             if(l.id === data.id){
                 setDetails(l);
             }
--- src/container/doctors/Doctors.js
+++ src/container/doctors/Doctors.js
@@ -4,12 +4,14 @@ import { NavLink } from 'react-router-dom';
 function Doctors(props) {
 
     const Data = [
-        { id: "1", name: "Marklin Lora", img: "../assets/img/doctors/doctors-1.jpg", designation: "Chief Medical Officer", description: "Duis sagittis rutrum neque, quis tincidunt arcu pretium ac.",ldescription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", phone: "98756 12345", add: "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016" },
-        { id: "2", name: "John White", img: "../assets/img/doctors/doctors-2.jpg", designation: "Anesthesiologist", description: "Aenean ac turpis ante. Mauris velit sapien.", ldescription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", phone: "98756 12345", add: "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016" },
-        { id: "3", name: "Umika Loha", img: "../assets/img/doctors/doctors-3.jpg", designation: "Cardiology", description: "Curabitur luctus eleifend odio. Phasellus placerat mi.", ldescription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", phone: "98756 12345", add: "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016" },
-        { id: "4", name: "Daimy Smith", img: "../assets/img/doctors/doctors-4.jpg", designation: "Neurosurgeon", description: "Morbi vulputate, tortor nec pellentesque molestie, eros nisi ornare purus.", ldescription:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", phone: "98756 12345", add: "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016" }
+        { id: "1", name: "Marklin Lora", img: "../assets/img/doctors/doctors-1.jpg", designation: "Chief Medical Officer", description: "Duis sagittis rutrum neque, quis tincidunt arcu pretium ac.", ldescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", phone: "98756 12345", add: "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016" },
+        { id: "2", name: "John White", img: "../assets/img/doctors/doctors-2.jpg", designation: "Anesthesiologist", description: "Aenean ac turpis ante. Mauris velit sapien.", ldescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", phone: "98756 12345", add: "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016" },
+        { id: "3", name: "Umika Loha", img: "../assets/img/doctors/doctors-3.jpg", designation: "Cardiology", description: "Curabitur luctus eleifend odio. Phasellus placerat mi.", ldescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", phone: "98756 12345", add: "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016" },
+        { id: "4", name: "Daimy Smith", img: "../assets/img/doctors/doctors-4.jpg", designation: "Neurosurgeon", description: "Morbi vulputate, tortor nec pellentesque molestie, eros nisi ornare purus.", ldescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", phone: "98756 12345", add: "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016" },
+        { id: "5", name: "Saimy Cmith", img: "../assets/img/doctors/doctors-5.jpg", designation: "Neurosurgeon", description: "Morbi vulputate, tortor nec pellentesque molestie, eros nisi ornare purus.", ldescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", phone: "98756 12345", add: "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016" }
     ]
 
+    localStorage.setItem("Doctor-Details", JSON.stringify(Data))
     return (
         <div>
             <section id="doctors" className="doctors">
@@ -23,12 +25,12 @@ function Doctors(props) {
                     <div className="row">
                         {
                             Data.map((d, i) => (
+                                <div className="col-6">
+                                    <NavLink to={{
+                                        pathname: "/doctor_details",
+                                        state: { id: d.id }
+                                    }}>
 
-                                <NavLink to={{
-                                    pathname: "/doctor_details",
-                                    state: {id: d.id}
-                                }}>
-                                    <div className="col-6">
                                         <div className="member d-flex align-items-start">
                                             <div className="pic"><img src={d.img} className="img-doctor" alt /></div>
                                             <div className="member-info">
@@ -43,8 +45,9 @@ function Doctors(props) {
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                </NavLink>
+
+                                    </NavLink>
+                                </div>
                             ))
                         }
                     </div>
